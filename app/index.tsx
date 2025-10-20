dc965a867d3971326674c1fb53d825bec98e9b82
Fernando Ortiz <ortizfernandomartin@gmail.com>
2025-10-20 12:15:46 +0200

    Start and Stop streaming

--- app/index.tsx
+++ app/index.tsx
@@ -24,46 +24,55 @@ export default function ChatScreen() {
   const isStreaming = useAppSelector(state => state.chat.isStreaming);
 
   const dispatch = useDispatch();
-
-  React.useEffect(() => {
-    const eventSource = new EventSource("https://api-dev.withallo.com/v1/demo/interview/conversation")
-    eventSourceRef.current = eventSource;
-
-    SUPPORTED_EVENTS.forEach(eventName =>
-      eventSource.addEventListener(eventName as any, (event: MessageEvent<string>) => {
-        const receivedEvent = {
-          event: event.type,
-          data: JSON.parse(event.data ?? '{}'),
-        };
   
-        switch (receivedEvent.event) {
-        case 'message_start':
-          dispatch(receivedMessageStart(receivedEvent.data));
-          break;
-        case 'text_chunk':
-          dispatch(receivedTextChunk(receivedEvent.data));
-          break;
-        case 'message_end':
-          dispatch(receivedMessageEnd());
-          break;
-        case 'component_start':
-          dispatch(receivedComponentStart(receivedEvent.data));
-          break;
-        case 'component_field':
-          dispatch(receivedComponentField(receivedEvent.data));
-          break;
-        case 'component_end':
-          dispatch(receivedComponentEnd());
-          break;
-        }
-      })
-    );
-
+  React.useEffect(() => {
+    const eventSource = new EventSource("https://api-dev.withallo.com/v1/demo/interview/conversation");
+    eventSourceRef.current = eventSource;
+    eventSource.close();
     return () => {
       eventSource.close();
       eventSourceRef.current = null;
     };
   }, []);
+
+  React.useEffect(() => {
+    const eventSource = eventSourceRef.current;
+    if (eventSource && isStreaming) {
+      eventSource.open();
+
+      SUPPORTED_EVENTS.forEach(eventName =>
+        eventSource.addEventListener(eventName as any, (event: MessageEvent<string>) => {
+          const receivedEvent = {
+            event: event.type,
+            data: JSON.parse(event.data ?? '{}'),
+          };
+
+          switch (receivedEvent.event) {
+          case 'message_start':
+            dispatch(receivedMessageStart(receivedEvent.data));
+            break;
+          case 'text_chunk':
+            dispatch(receivedTextChunk(receivedEvent.data));
+            break;
+          case 'message_end':
+            dispatch(receivedMessageEnd());
+            break;
+          case 'component_start':
+            dispatch(receivedComponentStart(receivedEvent.data));
+            break;
+          case 'component_field':
+            dispatch(receivedComponentField(receivedEvent.data));
+            break;
+          case 'component_end':
+            dispatch(receivedComponentEnd());
+            break;
+          }
+        })
+      );
+    } else {
+      eventSource?.removeAllEventListeners();
+    }
+  }, [isStreaming]);
 
   return (
     <ChatScreenContent 
